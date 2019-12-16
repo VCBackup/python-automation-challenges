2214509fed9675686cf9c3fff75f2b6cf0f9c112
daniel.moore@sling.com <daniel.moore@sling.com>
2019-12-16 12:02:14 -0700

    Completed challenge3.1

--- challenge3.1/challenge3.1.py
+++ challenge3.1/challenge3.1.py
@@ -1,10 +1,12 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium.webdriver.support.expected_conditions as ec
 
-# Go to Sling.com, Orange package, print the ALT text, and the URLs
 
-
-class challenge3(unittest.TestCase):
+class challenge3_1(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("../chromedriver.exe")
@@ -12,23 +14,15 @@ class challenge3(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-    def test_challenge3_for_loop(self):
-        # Navigate to Copart
-        self.driver.get("https://www.copart.com")
-        self.assertIn("Copart", self.driver.title)
-        print("Landed on Copart")
-        # Print a list of all "Popular Items > Makes/Models" & the URL/href for each type
-        for x in self.driver.find_elements_by_xpath("//*[contains(@ng-repeat, 'popularSearch')]"):
-            print(x.text+"- "+self.driver.find_element_by_link_text(x.text).get_attribute("href"))
-
-    def test_challenge3_while_loop(self):
-        # Navigate to Copart
-        self.driver.get("https://www.copart.com")
-        self.assertIn("Copart", self.driver.title)
-        print("Landed on Copart")
-        # Print a list of all "Popular Items > Makes/Models" & the URL/href for each type
-        for x in self.driver.find_elements_by_xpath("//*[contains(@ng-repeat, 'popularSearch')]"):
-            print(x.text+"- "+self.driver.find_element_by_link_text(x.text).get_attribute("href"))
+    def test_challenge3_1(self):
+        # Navigate to Sling.com
+        self.driver.get("https://www.sling.com")
+        self.assertIn("Sling TV", self.driver.title)
+        # Print the ALT text for the icons, and URLs
+        channel_icon_list = self.driver.find_elements_by_xpath("//*[@id=\"channelList\"]/li/img")
+        print("--------------- This is the list of channels ---------------")
+        for x in channel_icon_list:
+            print((x.get_attribute("alt") + ": \"" + x.get_attribute("src") + "\""))
 
 
 if __name__ == '__main__':
