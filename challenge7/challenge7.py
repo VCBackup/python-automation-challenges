eea213d97eb0021727f127ddef624700d3f9ae2a
Daniel <dmoore1525@gmail.com>
2021-05-02 17:42:32 -0600

    Updated to fix some bugs

--- challenge7/challenge7.py
+++ challenge7/challenge7.py
@@ -17,29 +17,57 @@ class challenge7(unittest.TestCase):
         self.driver.close()
 
     def test_challenge7(self):
-        list_of_elements = [
-            "//*[@ng-repeat=\"popularSearch in popularSearches | limitTo: 5\"]//a",
-            "//*[@ng-repeat=\"popularSearch in popularSearches | limitTo: 5 : 5\"]//a",
-            "//*[@ng-repeat=\"popularSearch in popularSearches | limitTo: 5: 10\"]//a",
-            "//*[@ng-repeat=\"popularSearch in popularSearches | limitTo: 5 : 15\"]//a"
-        ]
-        make_model_links = {}
+        make_text = []
+        make_links = []
+        model_text = []
+        model_links = []
+
         # Navigate to Copart
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
-        # Go to Makes/Models & store all the values displayed on the page along w/ URLs for the links
-        for x in list_of_elements:
-            make_model_list = self.driver.find_elements_by_xpath(x)
-            for y in make_model_list:
-                make_model_links[y.text] = y.get_attribute("href")
-        # Verify all the elements in the array navigate to the correct page
-        print(make_model_links)
-        for x in make_model_links:
+
+        # Store all the Make values displayed on the page along w/ URLs for the links
+        make_list = self.driver.find_elements_by_xpath("//*[@id=\"tabMakes\"]/div/div/div/span/span/a")
+        for x in make_list:
+            make_text.append(x.text)
+            make_links.append(x.get_attribute("href"))
+        t = 0
+        print("Makes")
+        while t < len(make_text):
+            print(make_text[t] + " : " + make_links[t])
+            t += 1
+        print("")
+
+        # Store all the Model values displayed on the page along w/ URLs for the links
+        self.driver.find_element_by_xpath("//*[@href=\"#tabModels\"]").click()
+        model_list = self.driver.find_elements_by_xpath("//*[@id=\"tabModels\"]/div/div/div/span/span/a")
+        for y in model_list:
+            model_text.append(y.text)
+            model_links.append(y.get_attribute("href"))
+        t = 0
+        print("Models")
+        while t < len(model_text):
+            print(model_text[t] + " : " + model_links[t])
+            t += 1
+
+        # Verify all the Make elements in the array navigate to the correct page
+        print("")
+        for i in make_links:
             try:
-                print("Opening Link for : " + str(x) + "")
-                self.driver.get(make_model_links[x])
+                print("Opening Link for : " + str(i) + "")
+                self.driver.get(i)
                 print("Success!")
-            except Exception:
+            except TimeoutError:
+                print("A page didn't load properly - Test Failed")
+
+        # Verify all the Model elements in the array navigate to the correct page
+        print("")
+        for i in model_links:
+            try:
+                print("Opening Link for : " + str(i) + "")
+                self.driver.get(i)
+                print("Success!")
+            except TimeoutError:
                 print("A page didn't load properly - Test Failed")
 
 
